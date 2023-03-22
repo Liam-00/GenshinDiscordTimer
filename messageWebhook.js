746f20a23d20bf333a0b4a93502bca19965a9903
Liam <osmanliam97@gmail.com>
2023-03-22 09:51:43 +0200

    FEAT: add new function

---
+++ messageWebhook.js
@@ -0,0 +1,23 @@
+//const dotenv = require('dotenv')
+import * as dotenv from 'dotenv';
+dotenv.config();
+const messageWebhook = async (input) => {
+    try {
+        let response = await fetch(process.env.WEBHOOK_URL + "?wait=true", {
+            method: "POST",
+            body: JSON.stringify({
+                content: input
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        });
+        let responsejson = await response.json();
+        return true;
+    }
+    catch (e) {
+        return false;
+    }
+};
+export { messageWebhook };
