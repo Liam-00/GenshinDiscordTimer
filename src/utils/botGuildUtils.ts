d655264e3298c79e557b2a097d21ed88937eb332
Liam <osmanliam97@gmail.com>
2023-11-18 10:38:05 +0200

    add bot guild utilities

---
+++ src/utils/botGuildUtils.ts
@@ -0,0 +1,11 @@
+import {Client} from 'discord.js'
+
+const getGuild = (client: Client) => {
+    let guild = client.guilds.cache.first()
+    if (!guild) {
+        throw new Error("Guild not found")
+    }
+    return guild
+}
+
+export {getGuild}
