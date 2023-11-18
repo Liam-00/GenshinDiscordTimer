2a3e06b4c231250e91dc23f048b939cfe60a0392
Liam <osmanliam97@gmail.com>
2023-11-18 10:16:32 +0200

    add bot settings

---
+++ src/types/BotSettings.ts
@@ -0,0 +1,4 @@
+export interface BotSettings {
+    bot_channel_name: string;
+    bot_user_role_name: string;
+}
---
+++ src/utils/botSettingUtils.ts
@@ -0,0 +1,18 @@
+import { fs } from 'fs'
+import type { BotSettings } from '../types/BotSettings'
+
+
+
+const getBotSettings = () => { 
+    let data = fs.readFileSync('./bot_config.json', {encoding: "utf-8"})
+    let settings_object: BotSettings = JSON.parse(data)
+
+    //enforce channel naming rules
+    settings_object.bot_channel_name = settings_object.bot_channel_name
+        .toLowerCase()
+        .replace(/[^a-z0-9_\s-]/g,"")
+        .replace(/\s/g, "-")
+
+
+    return settings_object
+}
