826b63475ec3e555d38fc2d95477a365987dd094
Liam <osmanliam97@gmail.com>
2023-11-18 15:00:48 +0200

    modify index to include new bot setup

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,75 @@
-import { messageWebhook } from "./messageWebhook.js";
-import { scrapeAndParseEvents, getSpiralAbyssEvent } from "./utils/eventDataUtils.js";
-import { timeRemaining, timeForReminder } from "./utils/timeUtils.js";
-import { createEventMessage } from "./discordMessageFormat.js";
+//build list of current events
+//     let eventlist = await scrapeAndParseEvents()
+//     eventlist.push(getSpiralAbyssEvent())// import { messageWebhook } from "./messageWebhook.js";
+// import { scrapeAndParseEvents, getSpiralAbyssEvent } from "./utils/eventDataUtils.js";
+// import { timeRemaining, timeForReminder } from "./utils/timeUtils.js";
+// import { createEventMessage } from "./discordMessageFormat.js";
 
-import type { GenshinEvent } from "./types/GenshinEvent.js";
-import type{ ReminderConfig } from "./types/ReminderConfig.js";
+// import type { GenshinEvent } from "./types/GenshinEvent.js";
+// import type{ ReminderConfig } from "./types/ReminderConfig.js";
 
-const msg = async () => {
-    //config reminder days
-    let reminderConf:ReminderConfig = {
-        reminderDays: [5, 2, 0]
+
+import {Client, GatewayIntentBits} from 'discord.js'
+import 'dotenv/config'
+import { createBotUserRole, getBotUserRoleFromGuild } from './utils/botUserRoleUtils'
+import { createBotChannel, getBotChannelFromGuild } from './utils/botChannelUtils'
+
+
+let client = new Client({intents: GatewayIntentBits.Guilds})
+
+client.on('ready', async () => {
+    console.log(`Bot is ready ${client.user?.tag}`)
+
+    let bot_user_role = getBotUserRoleFromGuild(client)
+    if (!bot_user_role) {
+        bot_user_role = await createBotUserRole(client)
     }
 
-    //build list of current events
-    let eventlist = await scrapeAndParseEvents()
-    eventlist.push(getSpiralAbyssEvent())
-
-    //init blank message
-    let embeds = []
-    //init reminder check
-    let performReminder: boolean = false;
-
-    //build message and check if today is a reminder day for any events
-    eventlist.forEach(event => {
-        embeds.push(createEventMessage(event)) 
-        if (timeForReminder(event, reminderConf)) {
-            performReminder = true;
-        }
-    })
-
-    //if reminder day, send message
-    if (performReminder) {
-        messageWebhook(embeds)
+    let bot_channel = getBotChannelFromGuild(client)
+    if (!bot_channel) {
+        bot_channel = await createBotChannel(client, bot_user_role)
     }
-}
+ 
+    //fetch new data generate message
 
-msg()
+    //clear channel
+
+    //send new message
+    
+    //send alert to channel if date conitions are met
+})
+
+client.login(process.env.BOT_TOKEN)
+
+
+
+// const msg = async () => {
+//     //config reminder days
+//     let reminderConf:ReminderConfig = {
+//         reminderDays: [5, 2, 0]
+//     }
+
+//     //build list of current events
+//     let eventlist = await scrapeAndParseEvents()
+//     eventlist.push(getSpiralAbyssEvent())
+
+//     //init blank message
+//     let embeds = []
+//     //init reminder check
+//     let performReminder: boolean = false;
+
+//     //build message and check if today is a reminder day for any events
+//     eventlist.forEach(event => {
+//         embeds.push(createEventMessage(event)) 
+//         if (timeForReminder(event, reminderConf)) {
+//             performReminder = true;
+//         }
+//     })
+
+//     //if reminder day, send message
+//     if (performReminder) {
+//         messageWebhook(embeds)
+//     }
+// }
+
+// msg()
