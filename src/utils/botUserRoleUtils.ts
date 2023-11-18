ee7befda673433a04d0ef0793d6f623948d8379f
Liam <osmanliam97@gmail.com>
2023-11-18 11:11:40 +0200

    add bot role utils

---
+++ src/utils/botUserRoleUtils.ts
@@ -0,0 +1,32 @@
+import { getBotSettings } from "./botSettingUtils"
+import { getGuild } from "./botGuildUtils"
+
+import type {Client, Role} from 'discord.js'
+ 
+const getBotUserRoleFromGuild = (client: Client): Role | null => {
+    let bot_settings = getBotSettings()
+
+    let roles = getGuild(client).roles.cache
+
+    let bot_user_role = roles.find(role => role.name === bot_settings.bot_user_role_name)
+
+    if (bot_user_role) {
+        return bot_user_role
+    }
+
+    return null
+}
+
+const createBotUserRole = async (client: Client) => {
+    let bot_settings = getBotSettings()
+
+    let role_manager = getGuild(client).roles
+
+    let role = await role_manager.create({
+        name: bot_settings.bot_user_role_name
+    })
+
+    return role
+}
+
+export {getBotUserRoleFromGuild, createBotUserRole}
