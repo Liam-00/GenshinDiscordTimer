172453e246aac80b8c9e354aa925ffa238477d33
Liam <osmanliam97@gmail.com>
2023-11-20 00:25:00 +0200

    fix regex error

--- src/utils/eventDataUtils.ts
+++ src/utils/eventDataUtils.ts
@@ -15,19 +15,21 @@ const scrapeAndParseEvents = async () : Promise<GenshinEvent[]> => {
         //query for rows in first tbody, then process through children
         events_tableHtml.querySelectorAll("tbody tr").forEach(item => {
             //find which rows are tagged In-Game events
+            
             if (item.childNodes[2].innerText.includes('In-Game')) {
                 
                 //get data for event date
-                let event_dateData:string[] = item.childNodes[1].innerText.split(/ &ndash; /)
+                let event_dateData:string[] = item.childNodes[1].innerText.split(/ – /)
                 
                 //get length of tabledata element that contains the event name
                 //then use it to select last child of that tabledata
                 let event_nameParentLength:number = item.childNodes[0].childNodes.length
                 let event_nameData:string = item.childNodes[0].childNodes[event_nameParentLength - 1].innerText
+                
 
                 let event_startDate:number = Date.parse(event_dateData[0])
                 let event_endDate:number = Date.parse(event_dateData[1])
-
+                
                 let event: GenshinEvent = {
                     name: event_nameData,
                     dateStart: event_startDate,
