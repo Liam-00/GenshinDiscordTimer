0bc617c37d05d9287370c23d7760b250678e875c
Liam <osmanliam97@gmail.com>
2023-11-19 19:35:59 +0200

    alter input types for sending message

--- src/utils/botMessageUtils.ts
+++ src/utils/botMessageUtils.ts
@@ -3,16 +3,20 @@ import { type APIEmbed, type Client, type MessageCreateOptions } from "discord.j
 
 
 
-const sendMessageToBotChannel = async (client:Client, message? : string, embedlist? : APIEmbed[]) => {
+const sendMessageToBotChannel = async (client:Client, message: string | APIEmbed[]) => {
     let channel = getBotChannelFromGuild(client)
     if (!channel) {
         throw new Error("Cannot access channel")
     }
 
-    let message_options: MessageCreateOptions = {
-        embeds: embedlist,
-        content: message
+    let message_options: MessageCreateOptions
+    if (typeof message === 'string') {
+        message_options = {content: message}
+    } else {
+        message_options = {embeds: message}
     }
     
     await channel.send(message_options)
 }
+
+export {sendMessageToBotChannel}
