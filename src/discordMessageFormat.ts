90ae7dd5cc7a07f5b4af991d0203b23ec9a75ac2
Liam-00 <93989398+Liam-00@users.noreply.github.com>
2023-11-24 13:57:49 +0200

    rework message formatting to match new time system

--- src/discordMessageFormat.ts
+++ src/discordMessageFormat.ts
@@ -7,22 +7,34 @@ const createEventMessage = (event: GenshinEvent): APIEmbed => {
     let event_title_buffer = '\\_'.repeat(50)
     let event_title = `**${event.name}:**`
 
-    //get remaining event time and format it
-    let timeleft = timeRemaining(event)
-    let event_timeLeft = `\`\`\`${timeleft.days} days ${timeleft.hours} hrs\`\`\``
+    //get remaining event time
+    let event_time_remaining = timeRemaining(event)
+
+    //build string for event time
+    let time_string = ``+
+    `${event_time_remaining.hours < 10 ? "0" : ""}${event_time_remaining.hours}:`+
+    `${event_time_remaining.minutes < 10 ? "0" : ""}${event_time_remaining.minutes}`
+
+    //build string for event days
+    let days_string = event_time_remaining.days === 0 ? `Today at`:
+        event_time_remaining.days === 1 ? `Tomorrow at` : `In ${event_time_remaining.days} Days at`
+
+    //assemble finished string
+    let event_timeLeft = `\`\`\`${days_string} ${time_string}\`\`\``
+    
     
     //formatted strings for event dates
     let eventstart = new Date(event.dateStart).toDateString()
     let eventend = new Date(event.dateEnd).toDateString()
 
     //get number of dashes ahead of and behind current day in timeine bar
-    let event_days_passed = Math.floor( (Date.now() - event.dateStart) / (1000 * 60 * 60 * 24) )
-    let event_days_ahead = Math.floor( (event.dateEnd - Date.now()) / (1000 * 60 * 60 * 24) )
+    let event_days_passed = Math.floor( (Date.now() - event.dateStart - (1000 * 60 * 60)) / (1000 * 60 * 60 * 24) )
+    let event_days_ahead = Math.floor( (event.dateEnd - (1000 * 60 * 60) - Date.now()) / (1000 * 60 * 60 * 24) )
     
     let event_timeline_bar = 
         '-'.repeat(event_days_passed) +
         '[-]' + 
-        '-'.repeat(event_days_ahead)
+        '-'.repeat(event_days_ahead + 1)
 
     let event_timeline = `\`\`\`${eventstart} ${event_timeline_bar} ${eventend}\`\`\``
     
@@ -32,7 +44,7 @@ const createEventMessage = (event: GenshinEvent): APIEmbed => {
         color: 16777215,
         fields: [
             {
-                name: `T i m e   L e f t :`,
+                name: `E v e n t  E n d s :`,
                 value: `${event_timeLeft}`,
                 inline: false
             },
