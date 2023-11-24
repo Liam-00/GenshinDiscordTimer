eb9f33f8a2b604ff77e7c5526d14b0995aef5148
Liam-00 <93989398+Liam-00@users.noreply.github.com>
2023-11-24 15:45:50 +0200

    remove unused config

--- src/types/BotSettings.ts
+++ src/types/BotSettings.ts
@@ -2,7 +2,6 @@ export interface BotSettings {
     bot_channel_name: string;
     bot_user_role_name: string;
     days_left_for_reminder: number[];
-    use_true_time: boolean;
     user_time_zone: number;
     server_time_zone: number;
     local_server_time_zone: number;
