5dfef4a03895b1934a38b4fd9f457a61f41eddbe
Liam <osmanliam97@gmail.com>
2023-12-01 16:45:34 +0200

    fix bash script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Running Bot..."
-cd $(dirname $0) && $(node run start) 1>"./logs/log" 2>"./logs/log_error"
-echo "...bot finished.
+cd $(dirname $0) && npm run start 1>./logs/log 2>./logs/log_error
+echo "...bot finished."
